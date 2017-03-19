5318299a0eaa46c15a18febb4b1c0ca85ae24c6c
Beth Nakamura <bnak@umich.edu>
2017-03-19 15:06:41 -0700

    MVP question 3, read JSON file and output to localhost

--- app/readJSON.js
+++ app/readJSON.js
@@ -1,15 +1,13 @@
 var fs = require('fs');
 
-function readJSON(){
+
+//synchronous
+function readJSON(filePath){
 
 	console.log("ReadJSON function")
 
-	content = fs.readFileSync("./resources/sample.json");
+	content = fs.readFileSync(filePath);
 	var obj = JSON.parse(content);
-//	fs.readFile('./resources/sample.json', 'utf8', function (err, data) {
-//	    if (err) throw err; // we'll not consider error handling for now
-//	    obj = JSON.parse(data);
-//	});
 
 	return obj;
 }
