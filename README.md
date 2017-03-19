99c45e17e42f0e7b720e7da8ed2570f6497c53ce
Beth Nakamura <bnak@umich.edu>
2017-03-19 13:31:35 -0700

    Serve HTTP to localhost

--- README.md
+++ README.md
@@ -4,3 +4,13 @@ Node.js Exercises
 
 Sources: 
 https://blog.risingstack.com/node-hero-tutorial-getting-started-with-node-js/
+
+https://blog.tompawlak.org/generate-random-values-nodejs-javascript
+
+http://stackoverflow.com/questions/1063007/how-to-sort-an-array-of-integers-correctly
+
+https://nodejs.org/api/readline.html
+
+https://howtonode.org/hello-node
+
+https://blog.xervo.io/build-your-first-http-server-in-nodejs
--- app/index.js
+++ app/index.js
@@ -1,5 +1,24 @@
-const order_random = require('./order_random')
+// QUESTION 3 3. Write a console app that reads a JSON document (you define
+// the structure of this document) from a file and outputs the properties of
+// the JavaScript object/object graph to the UI
 
 
-order_random.orderRandom10IntegerArray()
-// console.log(`The result is: ${result}`)  
+//Lets require/import the HTTP module
+var http = require('http');
+
+//Lets define a port we want to listen to
+const PORT=8080; 
+
+//We need a function which handles requests and send response
+function handleRequest(request, response){
+    response.end('It Works!! Path Hit: ' + request.url);
+}
+
+//Create a server
+var server = http.createServer(handleRequest);
+
+//Lets start our server
+server.listen(PORT, function(){
+    //Callback triggered when server is successfully listening. Hurray!
+    console.log("Server listening on: http://localhost:%s", PORT);
+});
