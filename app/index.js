3d07e476083d2cc28b775d5adfd4d8c736ba4e6c
Beth Nakamura <bnak@umich.edu>
2017-03-20 22:49:03 -0700

    Update readme and style guides

--- app/index.js
+++ app/index.js
@@ -6,26 +6,20 @@
 const readJSON = require('./readJSON');
 
 obj = readJSON.readJSON('app/resources/sample.json');
-console.log(obj);
 
-//Lets require/import the HTTP module
 var http = require('http');
 
-//Lets define a port we want to listen to
-const PORT=8080;
+const PORT=8081;
 
-//We need a function which handles requests and send response
+
 function handleRequest(request, response){
     response.setHeader('Content-Type', 'application/json');
     response.write(JSON.stringify(obj));
     response.end();
 }
 
-//Create a server
 var server = http.createServer(handleRequest);
 
-//Lets start our server
 server.listen(PORT, function(){
-    //Callback triggered when server is successfully listening. Hurray!
     console.log("Server listening on: http://localhost:%s", PORT);
 });
--- app/readJSON.js
+++ app/readJSON.js
@@ -4,12 +4,9 @@ var fs = require('fs');
 //synchronous
 function readJSON(filePath){
 
-	console.log("ReadJSON function");
-
 	content = fs.readFileSync(filePath);
 	var obj = JSON.parse(content);
-
 	return obj;
-}
+};
 
 module.exports.readJSON = readJSON;
