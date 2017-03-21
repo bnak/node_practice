0a31924aa8a986a38e4365651f7a559bf53df409
Beth Nakamura <bnak@umich.edu>
2017-03-20 21:04:05 -0700

    Q4 MVP

--- app/index.js
+++ app/index.js
@@ -1,31 +1,22 @@
-// QUESTION 3 
-// 3. Write a console app that reads a JSON document (you define
-// the structure of this document) from a file and outputs the properties of
-// the JavaScript object/object graph to the UI
+// QUESTION 4
+//4. Provide an http app that serves up a page that includes javascript that outputs the mouse coordinates
+//when clicked to the browser console.
 
-const readJSON = require('./readJSON');
 
-obj = readJSON.readJSON('app/resources/sample.json');
-console.log(obj);
 
-//Lets require/import the HTTP module
-var http = require('http');
 
-//Lets define a port we want to listen to
-const PORT=8080;
+var express = require('express');
+var app = express();
 
-//We need a function which handles requests and send response
-function handleRequest(request, response){
-    response.setHeader('Content-Type', 'application/json');
-    response.write(JSON.stringify(obj));
-    response.end();
-}
+app.use(express.static('public'));
+app.get('/', function (req, res) {
+   res.sendFile(__dirname  + "/" + "index.html" );
+})
 
-//Create a server
-var server = http.createServer(handleRequest);
 
-//Lets start our server
-server.listen(PORT, function(){
-    //Callback triggered when server is successfully listening. Hurray!
-    console.log("Server listening on: http://localhost:%s", PORT);
-});
+var server = app.listen(8081, function () {
+   var host = server.address().address
+   var port = server.address().port
+
+   console.log("Example app listening at http://localhost:%s",  port)
+})
